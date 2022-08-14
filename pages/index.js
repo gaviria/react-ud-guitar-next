88c6d1586d10afb91599db9ec2a41d6eb84da38d
Francisco <gaviria.reyes@gmail.com>
2022-08-14 13:50:51 -0500

    adding guitarras page and index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Layout from '../components/Layout'
+import Listado from '../components/Listado'
 
-export default function Home() {
+export default function Home({guitarras}) {
   return (
     <Layout page="Inicio">
-      <h1>Desde Inicio</h1>
+      <main className='contenedor'>
+        <h1 className="heading">Nuestra Colección</h1>
+        <Listado guitarras={guitarras}/>
+      </main>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras?populate=imagen&sort=createdAt%3Adesc`;
+  const respuesta = await fetch(url);
+  const guitarras = await respuesta.json();
+
+  console.log(guitarras);
+
+  return {
+      props:{
+          guitarras: guitarras
+      }
+  }
 }
